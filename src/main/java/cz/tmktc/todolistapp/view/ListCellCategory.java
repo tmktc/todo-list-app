60e828eafe89f2f3844389f3bf120e6db1483e9c
tmktc <tmktc.git@gmail.com>
2025-01-30 21:17:12 +0100

    add context menu to listView cells

--- src/main/java/cz/tmktc/todolistapp/view/ListCellCategory.java
+++ src/main/java/cz/tmktc/todolistapp/view/ListCellCategory.java
@@ -1,9 +1,16 @@
 package cz.tmktc.todolistapp.view;
 
+import cz.tmktc.todolistapp.controller.HomeController;
 import cz.tmktc.todolistapp.model.Category;
+import javafx.scene.control.ContextMenu;
 import javafx.scene.control.ListCell;
+import javafx.scene.control.MenuItem;
+
+import java.io.IOException;
 
 public class ListCellCategory extends ListCell<Category> {
+
+    HomeController homeController = new HomeController();
 
     @Override
     protected void updateItem(Category category, boolean empty) {
@@ -11,7 +18,22 @@ public class ListCellCategory extends ListCell<Category> {
         if (empty) setText(null);
         else {
             setText(category.getName());
+
+            MenuItem rename = new MenuItem("Rename");
+            MenuItem delete = new MenuItem("Delete");
+
+            rename.setOnAction(event -> {
+                try {
+                    homeController.renameCategory(category);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+            delete.setOnAction(event -> homeController.deleteCategory(category));
+
+            setContextMenu(new ContextMenu(rename, delete));
+
+
         }
     }
-
 }
--- src/main/java/cz/tmktc/todolistapp/view/ListCellTask.java
+++ src/main/java/cz/tmktc/todolistapp/view/ListCellTask.java
@@ -1,9 +1,16 @@
 package cz.tmktc.todolistapp.view;
 
+import cz.tmktc.todolistapp.controller.HomeController;
 import cz.tmktc.todolistapp.model.Task;
+import javafx.scene.control.ContextMenu;
 import javafx.scene.control.ListCell;
+import javafx.scene.control.MenuItem;
+
+import java.io.IOException;
 
 public class ListCellTask extends ListCell<Task> {
+
+    HomeController homeController = new HomeController();
 
     @Override
     protected void updateItem(Task task, boolean empty) {
@@ -11,6 +18,23 @@ public class ListCellTask extends ListCell<Task> {
         if (empty) setText(null);
         else {
             setText(task.getName());
+
+            MenuItem markCompleted = new MenuItem("Mark Completed");
+            MenuItem edit = new MenuItem("Edit");
+            MenuItem delete = new MenuItem("Delete");
+
+            markCompleted.setOnAction(event -> homeController.completeTask(task));
+            edit.setOnAction(event -> {
+                try {
+                    homeController.editTask(task);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+            delete.setOnAction(event -> homeController.deleteTask(task));
+
+            setContextMenu(new ContextMenu(markCompleted, edit, delete));
+
         }
     }
 
